9a611db9ef76faccc339bc8f9503918f3c4604f0
Louis <louisr@imagescape.com>
2011-06-29 14:44:06 -0500

    duplicate records created when logging in

--- authlog/logadminmiddleware.py
+++ authlog/logadminmiddleware.py
@@ -5,12 +5,7 @@ from authlog.decorators import watch_login, watch_view
 
 class LogAdminMiddleware(object):
 
-    def __init__(self, *args, **kwargs):
-        super(LogAdminMiddleware, self).__init__(*args, **kwargs)
-
-        # watch the admin login page
-        admin.site.login = watch_login(admin.site.login)
-
+    def process_request(self, request):
         # and the regular auth login page
         auth_views.login = watch_login(auth_views.login)
 
